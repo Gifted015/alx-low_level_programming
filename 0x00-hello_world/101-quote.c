8a64b3fcbfb350f9226871327318063f66d5137f
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-06-30 21:28:18 -0700

    Task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Prints using printf
+* Return: The input sentence
+*/
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
