0f39e736686454176621519e41200d51c7cf86eb
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-03 09:35:05 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Print whether the number n is positive or negative.
+* Description: This program will assign a random number to the variable n
+* Return: A string indicating whether n is positive or negative
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive", n);
+}
+else if (n < 0)
+{
+printf("%d is negative", n);
+}
+else if (n == 0)
+{
+printf("%d is zero", n);
+}
+return (0);
+}
