e2ff6d023cdb13d92a33e2dd62667f2e577accea
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-04 16:53:30 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,19 @@
 int main(void)
 {
 int x;
-for (x = '0'; x < '9'; x++)
+for (x = '0'; x <= '9'; x++)
 {
 putchar(x);
+if (x < '9')
+{
 putchar(',');
 putchar(' ');
 }
-putchar ('9');
+else
+{
+continue;
+}
+}
 putchar('\n');
 return (0);
 }
