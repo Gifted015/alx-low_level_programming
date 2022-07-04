4474148d86ad17a835b502479f778ca5269d85ab
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-04 05:54:02 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print the alphabets
+* Description - Prints the alphabets in lower case
+* Return: The alphabets
+*/
+int main(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
