957cf7625fd2cfc995cb3d49eef794b96bf1ce9f
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-05 15:16:13 -0700

    Task 3

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+/**
+* positive_or_negative - indicates whether number is positive or negative
+* @i: number to be checked
+* Return : positive or negative
+*/
+
 int positive_or_negative(int i)
 {
 int n;
@@ -17,5 +24,5 @@ else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
-return (0);
+return (positive_negative);
 }
