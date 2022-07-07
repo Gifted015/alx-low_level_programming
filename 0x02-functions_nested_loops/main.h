f634b01b5f20d8f4cc834a18fa2e739e6bf227d6
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-07 06:21:33 -0700

    Tasks

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,11 @@
 #define HEADER_FILE
 
 #include <stdio.h>
+int _abs(int n);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+int print_alphabet_x10(void);
 int print_alphabet(void);
 int print_last_digit(int);
 int _putchar(char c);
