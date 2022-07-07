36ae69abf1cabf8d7ed2742ce11227ccf92f9903
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-06 19:55:38 -0700

    Task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,20 +3,17 @@
 /**
  *_islower - checks for lowercase character.
  *Description: Returns 1 if c is lowercase, returns 0 otherwise
- *@c - character to be checked
+ *@c: character to be checked
  *Return: e
  */
-
 int _islower(int c)
 {
-int e;
 if (c >= 97 && c <= 122)
 {
-e = 49;
+return (1);
 }
 else
 {
-e = 48;
-} 
-return (e);
+return (0);
+}
 }
