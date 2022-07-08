c34f3a674e12d5cc8de6ec428ca25a28df954f54
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-08 16:22:13 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,17 +11,17 @@ long int i;
 long int x;
 long int r = 1;
 long int a = 2;
-for (x = 0; x <= 50; x++)
+for (x = 0; x < 50; x++)
 {
-printf ("%li", r);
+printf("%li", r);
 i = r + a;
 r = a;
 a = i;
-if (x < 50)
+if (x < 49)
 {
-printf (", ");
+printf(", ");
 }
 }
-printf ("\n");
+printf("\n");
 return (0);
 }
