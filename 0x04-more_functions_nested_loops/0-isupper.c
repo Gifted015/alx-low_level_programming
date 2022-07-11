e937a2ca484ec1be3e3efc171f5e1dac76bd7274
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-10 19:59:17 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for uppercase character
+ *Return: returns 1 if c is upper and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+int _isupper(int c);
+int _putchar(char c);
+
+#endif
