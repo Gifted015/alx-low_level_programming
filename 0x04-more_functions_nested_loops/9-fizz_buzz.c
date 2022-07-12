65c2d451dd9269fe4b48d799a0110fecad30b650
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-11 17:12:02 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,23 @@ for (x = 1; x <= 100; x++)
 {
 if (x % 3 == 0 && x % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (x % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (x % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
-printf("%d ", x);
+printf("%d", x);
+}
+if (x < 100)
+{
+printf(" ");
 }
 }
 printf("\n");
