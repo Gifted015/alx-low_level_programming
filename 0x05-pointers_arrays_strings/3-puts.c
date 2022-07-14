32cf6215bf55ab183e549d79538ab2f7502f9d4a
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-14 08:03:22 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,17 @@ void _puts(char *str)
 {
 long int a;
 int q = _strlen(str);
-for (a = 1; a <= q - 8; a++)
+for (a = 1; a <= q; a++)
 {
-if (*str == '\n')
-break;
+if (*str != '\0')
+{
 _putchar(*str);
-str++;
+str = str + 1;
+}
+else
+{
+break;
+}
 }
 _putchar('\n');
 }
