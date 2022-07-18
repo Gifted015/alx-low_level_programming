f523fa983b34446cc115395ce72c43b0c51255a9
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-18 07:25:47 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,28 @@
 #include "main.h"
-
+#include <string.h>
 /**
  *_strcat - concatenates two strings
  *Description: appends the src string to the dest string
+ *@dest: string to be apoended to
+ *@src: source string
  *Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-
-while (*src != '\0')
+int i;
+int q = strlen(dest) + strlen(src);
+for (i = 0; i < q; i++)
 {
-if (*dest != '\0')
+if (dest[i] != '\0')
 {
-dest++;
+i++;
 }
 else
 {
-*dest = *src;
-dest++;
+*(dest + i) = *src;
 src++;
 }
 }
-return (src);
+return (dest);
 }
