a92848775189252c4b187a8c1d391d17daa63ff3
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-19 16:31:58 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
  *cap_string - capitalizes all words of a string
- *Description: Separators of words: space tabulation new line , ; . ! ? " ( ) { and }
+ *Description: Separatorsspace tabulation new line , ; . ! ? " ( ) { and }
  *@a: string
  *Return: a
  */
@@ -12,8 +12,29 @@ char *cap_string(char *a)
 int b;
 for (b = 0; a[b] != '\0'; b++)
 {
-if ((a[b] > 96 && a[b] < 123) && (a[b - 1] == 9 || a[b - 1] == ' ' || a[b - 1] == '\n' || a[b - 1] == ',' || a[b - 1] == ';' || a[b - 1] == '.' || a[b - 1] == '!' || a[b - 1] == '?' || a[b - 1] == '"' || a[b - 1] == '(' || a[b - 1] == ')' || a[b - 1] == '{' || a[b - 1] == '}'))
+if (a[b] > 96 && a[b] < 123)
+{
+if (b == 0)
+{
 a[b] = a[b] - 32;
+}
+if (a[b - 1] == 9 || a[b - 1] == ' ' || a[b - 1] == '\n' || a[b - 1] == ',')
+{
+a[b] = a[b] - 32;
+}
+if (a[b - 1] == ';' || a[b - 1] == '.' || a[b - 1] == '!' || a[b - 1] == '?')
+{
+a[b] = a[b] - 32;
+}
+if (a[b - 1] == '"' || a[b - 1] == '(' || a[b - 1] == ')')
+{
+a[b] = a[b] - 32;
+}
+if (a[b - 1] == '{' || a[b - 1] == '}')
+{
+a[b] = a[b] - 32;
+}
+}
 }
 return (a);
 }
