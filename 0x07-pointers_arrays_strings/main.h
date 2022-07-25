7d14b0677859dbc79bda17d0a72f85352f151f60
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-25 15:41:56 -0700

    Task 3

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define HEADER_FILE
 
 #include <stdio.h>
+unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
