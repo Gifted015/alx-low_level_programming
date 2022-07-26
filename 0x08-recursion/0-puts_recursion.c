fbfc3d7a47fdd3d809651baa720fb00dd97bfeb2
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-26 14:55:23 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,30 @@
 #include "main.h"
 
 /**
+ *_puts - return a string
+ *@s: string
+ *Return: string
+ */
+
+char _puts(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return (0);
+}
+else
+_putchar(*s);
+return(_puts(s + 1));
+}
+
+
+/**
  *_puts_recursion - prints a string
  *@s: string
  */
 
 void _puts_recursion(char *s)
 {
-int a;
-for (a = 0; s[a] != '\0'; a++)
-{
-_putchar(s[a]);
-}
-_putchar('\n');
+_puts(s);
 }
