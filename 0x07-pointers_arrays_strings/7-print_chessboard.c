bcf706ff15998c56c25a41c3dbbc3142dc1a615f
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-26 05:48:20 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_print_chessboard - prints the chessboard
+ *@a: content
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int b, c = 0;
+for (b = 0; a[b][c] != '\0'; b++)
+{
+for (c = 0; c < 8; c++)
+{
+_putchar(a[b][c]);
+if (c == 7)
+_putchar('\n');
+}
+}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define HEADER_FILE
 
 #include <stdio.h>
+void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
