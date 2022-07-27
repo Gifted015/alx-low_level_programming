835678b7565462b83e03e743d0852b79b3077c15
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-07-27 11:50:51 -0700

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,82 @@
+#include "main.h"
+/**
+ *_ass - assigns a string to a buffer
+ *@s: string
+ *@a: buffer
+ *@n: number of the string
+ *Return: end of string (s)
+ */
+
+char *_ass(char *s, char *a, int *n)
+{
+if (*s == '\0')
+{
+return (s - 1);
+}
+else
+*n = *n + 1;
+*a = *s;
+return (_ass((s + 1), (a + 1), n));
+}
+
+
+/**
+ *_rev - reverses a string and assigns it to a buffer
+ *Description: Will only function well after function _ass as been called
+ *@s: string
+ *@a: buffer
+ *Return: buffer
+ */
+
+char *_rev(char *s, char *a)
+{
+if (*s == '\0')
+{
+return (a);
+}
+else
+*a = *s;
+return (_rev((s - 1), (a + 1)));
+}
+
+
+/**
+ *_palindrome - compares two strings
+ *@i: 1st string
+ *@j: 2nd string
+ *@k: int to count the similar characters
+ *Return: k
+ */
+
+int _palindrome(char *i, char *j, int k)
+{
+if (*i == '\0' || *j == '\0')
+return (k);
+
+else if (*i == *j)
+k = k + 1;
+
+return (_palindrome((i + 1), (j + 1), k));
+}
+
+
+/**
+ *is_palindrome - checks if a string is a palindrome
+ *@s: string
+ *Return: 1 if s is a palindrome otherwise returns 0
+ */
+
+int is_palindrome(char *s)
+{
+int n[] = {0};
+int r, k = 0;
+char i[100], j[100];
+s = _ass(s, i, n);
+_rev(s, j);
+r = _palindrome(i, j, k);
+if (r == *n)
+return (1);
+
+else
+return (0);
+}
