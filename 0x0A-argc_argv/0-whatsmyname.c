54520f57c155b20f67b367ee772a2b81894b8112
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-01 14:52:00 -0700

    TAsk 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,13 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-__attribute__((unused)) int i;
+int i;
+int j = 0;
 for (i = 0; i < argc; i++)
-_puts(argv[i]);
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+_putchar(argv[i][j]);
+_putchar('\n');
+}
 return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,11 +1,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stdio.h>
-#include "2-strlen.c"
-#include "3-puts.c"
+#include "_putchar.c"
 
-int _strlen(char *s);
-void _puts(char *s);
 int main(int argc __attribute__((unused)), char *argv[]);
+int _putchar(char c);
 
 #endif
