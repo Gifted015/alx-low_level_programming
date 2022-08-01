c0927a950260a8b3d3d47e2438bae49ef829d604
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-01 14:25:06 -0700

    Task 0

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,8 +5,9 @@
 #include "2-strlen.c"
 #include "3-puts.c"
 
-
+int _strlen(char *s);
 void _puts(char *s);
+int _putchar(char c);
 int main(int argc __attribute__((unused)), char *argv[]);
 
 #endif
