83b35899f42ca6f1341b1dda856b20c895ddffd8
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-03 09:14:01 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,10 +12,10 @@ char *str_concat(char *s1, char *s2)
 {
 unsigned int i, j;
 char *A;
-if (s1 == NULL || s2 == NULL)
-{
-return (NULL);
-}
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 else
 {
 for (i = 0; s1[i] != '\0'; i++)
