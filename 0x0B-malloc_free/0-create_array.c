9270f0fdb662934d47e5dd97b9d7c88f61952dc7
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-03 06:58:55 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *create_array - creates an array of chars, and initializes it with a specific char
+ *create_array - creates an array of chars,
+ *and initializes it with a specific char
  *@size: size of array
  *@c: character to be initialized into array
  *Return: NULL if sixe = 0 else returns pointer to the array
