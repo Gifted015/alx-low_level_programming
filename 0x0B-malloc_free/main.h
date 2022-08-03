64392fffb82063c72a465ec4cf5a787fcc3192a5
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-03 12:48:30 -0700

    Task 4

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
