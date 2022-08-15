0c68dbbe5a0bba9599b53a815ae8f37a171d30a1
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-15 08:48:00 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef Dog
+#define Dog
+
+/**
+ *struct dog - deines new types for dog names
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
