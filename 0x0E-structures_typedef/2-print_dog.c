37d19a2b54a0eb0ea586a877d9eff2affa2c4829
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-15 13:44:22 -0700

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,19 @@
 
 void print_dog(struct dog *d)
 {
+if (d != NULL)
+{
+if (d->name == NULL)
+d->name = "(nil)";
+
+if (d->age == '\0')
+d->age = 0;
+
+if (d->owner == NULL)
+d->owner = "(nil)";
+
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
+}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,7 @@
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
 #ifndef Dog
 #define Dog
 
