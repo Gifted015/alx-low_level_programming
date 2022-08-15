368f8f36d5e72308c95efe1bf81d832429f3855c
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-15 14:36:20 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+
+/**
+ *new_dog - creates a new dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *Return: new_dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+struct dog d;
+dog_t *r;
+
+if (name == NULL)
+return (NULL);
+
+if (age == '\0')
+return (NULL);
+
+if (owner == NULL)
+return (NULL);
+   
+else
+{
+d.name = name;
+d.age = age;
+d.owner = owner;
+r = &d;
+}
+return (r);
+}
