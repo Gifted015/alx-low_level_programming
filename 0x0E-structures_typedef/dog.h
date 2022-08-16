67732a8255e9f2b53d6b1997f414407df546748b
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-16 14:59:04 -0700

    Task 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,6 +24,7 @@ char *owner;
  */
 typedef struct dog dog_t;
 
+void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
