b22939dbd4dc13d19c68f5e73e9f4b0ae68daf82
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-17 15:07:34 -0700

    Task 2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 
