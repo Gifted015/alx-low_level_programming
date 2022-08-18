d9cabbadebfbddd546a0436087e24460f20552ac
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-08-18 15:46:50 -0700

    Task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 
