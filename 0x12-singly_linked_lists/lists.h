f087849aac82e04d72b1740db210a9f5b5540cf3
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-09-28 07:36:10 -0700

    Task 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,6 +23,7 @@ struct list_s *next;
 } list_t;
 
 
+list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 int _putchar(char c);
