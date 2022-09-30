55dce90fb142ddaf9a557f2c4c2d3a78effa07ff
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-09-30 14:04:54 -0700

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,8 +13,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new = malloc(sizeof(dlistint_t));
 
-if (new == NULL || *head == NULL)
-return (NULL);
+if (new == NULL)
+free(new), return (NULL);
 
 new->n = n;
 new->prev = NULL;
