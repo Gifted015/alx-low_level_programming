7c477db32982310fae869241c2ab4602ec533b62
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-11-25 16:48:12 -0800

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include main.h
+
+
+/**
+ *print_binary - prints the binary representation of a number
+ *@n: binary representation
+ *Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+  
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+void print_binary(unsigned long int n);
 int _pow(int a, int b);
 unsigned int binary_to_uint(const char *b);
 
