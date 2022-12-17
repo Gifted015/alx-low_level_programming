acc392aca8ceb85eb6c7c851dd62589f8927e54a
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-12-17 12:25:49 +0100

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ *get_bit - returns the value of a bit at a given index
+ *@n: the number to converted to bit
+ *@index: the index, starting from 0 of the bit you want to get
+ *Return: the value of the bit at the index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+return(n << index);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 int _pow(int a, int b);
 unsigned int binary_to_uint(const char *b);
