0e8cca443e44099fc0b9f76e0f638992817c13bd
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-12-20 23:34:22 +0100

    Task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  *set_bit - sets the value of a bit to 1 at a given index
  *@n: number to be edited
  *@index: the index, starting from 0 of the bit to be set
- *Return 1 if it worked, or -1 if an error occured
+ *Return: 1 if it worked, or -1 if an error occured
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
@@ -13,7 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 __attribute__((unused))  unsigned long int temp;
 
 if (index > 64)
-return(-1);
+return (-1);
 
 else
 {
