00b5e76a8dff6463fa54b41efa6d05a1fe68f223
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-12-29 21:39:25 +0100

    Task 5

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int clear_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
