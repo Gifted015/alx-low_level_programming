1f1551719e4e799b3929bcaf2a0f340873098743
Omoyeni Oreoluwani <omongprec@gmail.com>
2022-12-29 21:52:20 +0100

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,7 +2,8 @@
 
 
 /**
- *flip_bits - calculates how many number of bits you would need to flip to get from one number to another
+ *flip_bits - calculates how many number of bits you would need to flip
+ *to get from one number to another
  *@n: number to be fliped
  *@m: number to get from the flipping
  *Return: the number of bit to be flipped
@@ -11,10 +12,13 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned int num = 0;
+
 while (n >> 0)
 {
 if ((n & 1) != (m & 1))
 num++;
 n = n >> 1;
-m = m >> 1; }
-return (num); }
+m = m >> 1;
+}
+return (num);
+}
