23c13b8be9b33457755c95739029d980c1abbfe6
Omoyeni Oreoluwani Precious <omongprec@gmail.com>
2023-01-09 00:41:23 +0100

    Task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
 /**
  *print_listint - prints all the elements of a listint_t list
  *@h: the list to be printed
- *Return: the nuber of nodea
+ *Return: the number of nodes
  */
 
 size_t print_listint(const listint_t *h)
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,6 +21,7 @@ int n;
 struct listint_s *next;
 } listint_t;
 
+size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 int _putchar(char c);
 
