747d49b8498e20de8ff211e8f9c34ecd33047b60
Omoyeni Oreoluwani Precious <omongprec@gmail.com>
2023-01-15 20:26:07 +0100

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+/**
+ *free_listint - frees a listint_t list
+ *@head: the list yo be freed
+ *Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+  free(head);
+}
