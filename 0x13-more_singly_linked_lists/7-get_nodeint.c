c2c8e91bb042c395b126a4938873a895a7f1fc31
Omoyeni Oreoluwani Precious <omongprec@gmail.com>
2023-05-14 00:02:22 +0100

    Print Node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 
 
 /**
- *get_nodeint_at_index - function that 
+ *get_nodeint_at_index - function that
  *@head: the listint_t linked list
  *@index: the index of the node, starting at 0
  *Return: the nth node of a listint_t linked list
@@ -14,11 +14,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 unsigned int x;
 listint_t *temp = head;
 
-for(x = 0; x < index; x++)
+for (x = 0; x < index; x++)
 {
 if (temp == NULL)
 break;
 temp = temp->next;
 }
-return temp;
+ return (temp);
 }
