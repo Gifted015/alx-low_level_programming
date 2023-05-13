0909b3ac0396c54e1d48dd8a73f9bb6888ebfb3b
Omoyeni Oreoluwani Precious <omongprec@gmail.com>
2023-05-14 00:00:33 +0100

    Print Node

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,6 +21,7 @@ int n;
 struct listint_s *next;
 } listint_t;
 
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int pop_listint(listint_t **head);
 void free_listint2(listint_t **head);
 void free_listint(listint_t *head);
